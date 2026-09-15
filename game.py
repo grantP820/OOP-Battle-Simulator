ec7c312dd047500b56f501a4d80ac8b1ff311fcc
grantP820 <grant.porter@ascte.org>
2026-09-15 14:06:39 -0500

    added a battle

--- game.py
+++ game.py
@@ -3,6 +3,18 @@ from hero import Hero
 
 ARENA_NAME = "cracker barrel"
 
+def battle(hero: Hero,enemy: Goblin):
+    while hero.is_alive() and enemy.is_alive():
+        hero_damage = hero.attack()
+        enemy.take_damage(hero_damage)
+
+        if enemy.is_alive():
+            enemy_damage = enemy.attack()
+            hero.take_damage(enemy_damage)
+    if hero.is_alive():
+        print(f"{hero.name} Wins")
+    else:
+        print(f"{enemy.name} WIns")
 
 def main():
     """Open the arena and introduce its first opponent."""
@@ -17,8 +29,9 @@ def main():
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
     print("But no hero has answered the call... yet.")
-    jimmyAttack = jimmy.attack()
-    goblin.take_damage(jimmyAttack)
+    jimmy=Hero("Jimmy")
+    battle(jimmy,goblin)
+
 
 
 if __name__ == "__main__":
--- hero.py
+++ hero.py
@@ -3,14 +3,17 @@ class Hero:
     def __init__(self, name):
             self.name = name
             self.health = 150
-            self.attack_power = 20
+            self.attack_power = 25
     def attack(self):
-            return random.randint(10, self.attack_power)
+            return random.randint(3, self.attack_power)
     def take_damage(self, damage):
-            self.health = max(0, self.health - damage)
+            self.health = self.health - damage
+            if self.health < 0:
+                   self.health = 0
             print(f"{self.name} takes {damage} damage. Health: {self.health}")
     def is_alive(self):
             return self.health > 0
     def battleCry(self):
            print("hero screams out to goblins intimiating the goblins")
+
     __init__
